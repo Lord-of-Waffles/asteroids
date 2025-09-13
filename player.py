5490b8e3caa3a0dfc76e4bc5e876588c0b19e657
Lord-of-Waffles <ben.worton@gmail.com>
2025-09-13 16:34:13 +0200

    created player rectangle

--- player.py
+++ player.py
@@ -4,9 +4,8 @@ from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS )
         self.rotation = 0
-        self.radius = PLAYER_RADIUS
     
     def triangle(self):
         forward = pygame.Vector2(0,1).rotate(self.rotation)
@@ -15,3 +14,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
